cbd7ea23425b9f99567df689673be1945677ff2d
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-25 12:57:03 +0300

    Add task create view and endpoint

--- task/urls.py
+++ task/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from task.views import (
     index,
+    TaskCreateView,
 )
 
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from task.models import Task
 
@@ -10,3 +12,9 @@ def index(request: HttpRequest) -> HttpResponse:
         "tasks": tasks
     }
     return render(request, "task/index.html", context=context)
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task:index")
