e962c9b4c1fb368429301ffe31ee376da8b7daea
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-25 13:23:26 +0300

    Add Task update view and update path to urls.py

--- task/urls.py
+++ task/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from task.views import (
     index,
     TaskCreateView,
+    TaskUpdateView,
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -19,3 +19,9 @@ class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("task:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("task:index")
