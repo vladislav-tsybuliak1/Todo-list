de83d11cc03a0cbd5164fc1a2ade721371a3a145
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-25 10:42:56 +0300

    Add Task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline_at = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField()
+    tags = models.ManyToManyField(
+        to="Tag",
+        related_name="tasks",
+    )
+
