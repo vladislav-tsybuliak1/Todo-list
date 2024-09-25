700d3639db8f2d73848e3988aadcac9826e9763d
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-25 13:06:04 +0300

    Add cripsy forms to installed apps

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "debug_toolbar",
     "task",
 ]
@@ -132,3 +134,9 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django crispy forms settings
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
